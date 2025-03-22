5e1852824f450644b3c48315b35e4e8ddb45bda6
Maike <maikerdralcantara@gmail.com>
2025-03-22 12:17:51 -0300

    Enable plugins

--- lua/plugins/ray-x.lua
+++ lua/plugins/ray-x.lua
@@ -1,16 +1,14 @@
-return { 
+return {
   {
     "ray-x/go.nvim",
-    dependencies = {  -- optional packages
+    dependencies = { -- optional packages
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = function()
-      require("go").setup()
-    end,
-    event = {"CmdlineEnter"},
-    ft = {"go", 'gomod'},
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-  }
+    config = function() require("go").setup() end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }
